30994170903420fe3456e850f999ca56c1c5b948
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-05-20 01:21:43 -0300

    adicionando um estilo para prioridade das tarefas

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,15 @@
 import { taskRepository } from "@/repositories/task";
+import clsx from "clsx";
 import { BadgeCheck, BadgeX } from "lucide-react";
 
 export default async function HomePage() {
   const tasks = await taskRepository.findAll();
 
   return (
-    <div className="px-2">
-      <h1 className="text-2xl font-extrabold">Olá, Gabriel Enzo</h1>
+    <div className="px-4">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl font-extrabold">
+        Olá, Gabriel Enzo
+      </h1>
       <ul className="list-none">
         {tasks.map((task) => (
           <li key={task.id}>
@@ -16,13 +19,28 @@ export default async function HomePage() {
               </h2>
               <p className="px-4">{task.content}</p>
 
-              <div className="flex justify-between pt-1 pb-1 px-4 rounded-b-2xl bg-gray-300">
+              <div className="flex items-center justify-between pt-1 pb-1 px-4 rounded-b-2xl bg-gray-300">
                 <div className="flex gap-1">
-                  <p>{task.priority}</p>
+                  <p className="my-1 ">Prioridade:</p>
+                  <label
+                    className={clsx("my-1 rounded w-20 text-center", {
+                      "bg-red-500": task.priority === "alta",
+                      "bg-yellow-500": task.priority === "média",
+                      "bg-blue-500": task.priority === "baixa",
+                    })}
+                  >
+                    {task.priority}
+                  </label>
                 </div>
 
                 <div>
-                  <p>{task.done ? <BadgeCheck /> : <BadgeX />}</p>
+                  <p>
+                    {task.done ? (
+                      <BadgeCheck className="fill-green-500" />
+                    ) : (
+                      <BadgeX className="fill-red-500" />
+                    )}
+                  </p>
                 </div>
               </div>
             </div>
