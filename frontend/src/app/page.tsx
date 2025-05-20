29bec6a8fc1563d5a4e28e718bd4e9596b014b71
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-05-20 14:07:10 -0300

    adicionando filtro de tarefas com dados mockados

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,52 @@
+import { FilterTasks } from "@/components/FilterTasks";
 import { TasksList } from "@/components/TasksList";
 import { taskRepository } from "@/repositories/task";
 
-export default async function HomePage() {
-  const tasks = await taskRepository.findAll();
+type HomePageProps = {
+  searchParams: {
+    filter?: "all" | "done" | "pending";
+  };
+};
+
+export default async function HomePage({ searchParams }: HomePageProps) {
+  const filter = searchParams?.filter || "all";
+
+  let tasks;
+
+  switch (filter) {
+    case "done":
+      tasks = await taskRepository.findDone();
+      break;
+    case "pending":
+      tasks = await taskRepository.findPending();
+      break;
+    case "all":
+      tasks = await taskRepository.findAll();
+  }
 
   return (
     <div className="px-4">
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-extrabold">
         Olá, Gabriel Enzo
       </h1>
+
+      <div className="flex gap-2 my-4">
+        <FilterTasks
+          linkProps={{ href: "/?filter=all" }}
+          labelFilter="Todas"
+          isActive={filter === "all"}
+        />
+        <FilterTasks
+          linkProps={{ href: "/?filter=pending" }}
+          labelFilter="Pendentes"
+          isActive={filter === "pending"}
+        />
+        <FilterTasks
+          linkProps={{ href: "/?filter=done" }}
+          labelFilter="Concluídas"
+          isActive={filter === "done"}
+        />
+      </div>
       <TasksList tasks={tasks} />
     </div>
   );
