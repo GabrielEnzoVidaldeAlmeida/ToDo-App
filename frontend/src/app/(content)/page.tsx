6e12cfcc718135c0ceebea49d003a034a0d774fc
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-05-21 16:55:55 -0300

    adicionando botão de adicionar tarefa

--- frontend/src/app/(content)/page.tsx
+++ frontend/src/app/(content)/page.tsx
@@ -1,6 +1,8 @@
 import { FilterTasks } from "@/components/FilterTasks";
 import { TasksList } from "@/components/TasksList";
 import { taskRepository } from "@/repositories/task";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
 
 type HomePageProps = {
   searchParams: {
@@ -26,22 +28,32 @@ export default async function HomePage({ searchParams }: HomePageProps) {
 
   return (
     <div className="flex flex-col">
-      <div className="flex justify-center sm:justify-start gap-2 my-4">
-        <FilterTasks
-          linkProps={{ href: "/?filter=all" }}
-          labelFilter="Todas"
-          isActive={filter === "all"}
-        />
-        <FilterTasks
-          linkProps={{ href: "/?filter=pending" }}
-          labelFilter="Pendentes"
-          isActive={filter === "pending"}
-        />
-        <FilterTasks
-          linkProps={{ href: "/?filter=done" }}
-          labelFilter="Concluídas"
-          isActive={filter === "done"}
-        />
+      <div className="flex flex-col sm:flex-row items-center justify-between">
+        <div className="flex justify-center sm:justify-start gap-2 my-4">
+          <FilterTasks
+            linkProps={{ href: "/?filter=all" }}
+            labelFilter="Todas"
+            isActive={filter === "all"}
+          />
+          <FilterTasks
+            linkProps={{ href: "/?filter=pending" }}
+            labelFilter="Pendentes"
+            isActive={filter === "pending"}
+          />
+          <FilterTasks
+            linkProps={{ href: "/?filter=done" }}
+            labelFilter="Concluídas"
+            isActive={filter === "done"}
+          />
+        </div>
+
+        <Link
+          href="#"
+          className="flex items-center gap-2 border-1 px-3 py-1 rounded bg-gray-200 hover:brightness-90 transition"
+        >
+          <PlusIcon className="w-4 h-4" />
+          Adicionar tarefa
+        </Link>
       </div>
       <TasksList tasks={tasks} />
     </div>
--- frontend/src/components/MainContainer/index.tsx
+++ frontend/src/components/MainContainer/index.tsx
@@ -3,9 +3,5 @@ type MainContainerProps = {
 };
 
 export function MainContainer({ children }: MainContainerProps) {
-  return (
-    <div className="flex flex-1 justify-center items-center pb-10 px-5 lg:px-35">
-      {children}
-    </div>
-  );
+  return <div className="flex flex-1 pb-10 px-5 lg:px-38">{children}</div>;
 }
