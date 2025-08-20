23eb5199fb2711c8e409897e1f5f916d7fb97b7f
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-08-20 20:02:04 -0300

    refactor: remove unnecessary function from the system

--- frontend/src/components/Nav/index.tsx
+++ frontend/src/components/Nav/index.tsx
@@ -1,31 +1,10 @@
 "use client";
 
-import clsx from "clsx";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
 
 export function Nav() {
-  const [handleMenu, setHandleMenu] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setHandleMenu(false);
-      }
-    }
-
-    if (handleMenu) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [handleMenu]);
+  //TODO:Utilizar CLSX
 
   return (
     <nav className="flex justify-between items-center bg-black text-white p-4">
@@ -38,37 +17,10 @@ export function Nav() {
         </Link>
       </h1>
 
-      <div className="relative flex items-center" ref={menuRef}>
-        <button onClick={() => setHandleMenu((prev) => !prev)}>
-          <Image
-            src="/images/icon-teste.webp"
-            alt="icon"
-            width={50}
-            height={50}
-            className={clsx(
-              "rounded border-2 border-gray-200 object-cover hover:brightness-80 transition cursor-pointer",
-              "w-8 h-8 sm:h-10 sm:w-10 md:w-12 md:h-12"
-            )}
-            priority
-          />
-        </button>
-
-        {handleMenu && (
-          <div className="absolute top-full right-0 mt-2 flex flex-col bg-white pr-8 pl-3 py-1 gap-1 underline z-50 shadow-lg rounded md:pr-12 md:pl-4">
-            <Link
-              href="#"
-              className="text-black hover:decoration-black hover:text-blue-700"
-            >
-              Perfil
-            </Link>
-            <Link
-              href="#"
-              className="text-black hover:decoration-black hover:text-blue-700"
-            >
-              Sair
-            </Link>
-          </div>
-        )}
+      <div className="relative flex items-center">
+        <h1 className="text-lg sm:text-2xl md:text-2xl lg:text-3xl font-extrabold bg-white text-black px-2 rounded">
+          Mesa da Vovó
+        </h1>
       </div>
     </nav>
   );
