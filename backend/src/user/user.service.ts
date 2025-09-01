d166b421629264c3a60e12d72d06649be7cd57d1
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-09-01 16:04:13 -0300

    feat: add hashingService for password

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -3,18 +3,22 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { HashingService } from 'src/common/hashing/hashing.service';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
   ) {}
 
   async create(dto: CreateUserDto) {
+    const hashedPassword = await this.hashingService.hash(dto.password);
+
     const newUser: CreateUserDto = {
       name: dto.name,
-      password: dto.password,
+      password: hashedPassword,
     };
 
     const created = await this.userRepository.save(newUser);
