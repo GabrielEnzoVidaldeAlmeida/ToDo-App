d765e30d71b072e3feaa8eefac18aa73aa11fbe3
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-09-01 15:14:50 -0300

    feat: add ConfigModule and TypeOrm configuration using env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: process.env.DB_DATABASE || './db.sqlite',
+      synchronize: process.env.DB_SYNCHRONIZE === '1',
+      autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
