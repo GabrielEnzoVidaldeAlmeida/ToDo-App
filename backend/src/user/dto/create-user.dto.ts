45b1ebf052175f449b65bdf0e88d62ed444dacef
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-09-15 15:32:36 -0300

    feat: create update password feature

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,11 +1,12 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Nome precisa ser uma string' })
+  @IsNotEmpty({ message: 'Nome não pode ser vazio' })
   name: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Senha precisa ser uma string' })
+  @IsNotEmpty({ message: 'Senha não pode ser vazia' })
+  @MinLength(6, { message: 'Senha deve ter um mínimo de 6 caracteres' })
   password: string;
 }
